76bec29bc5e8316fe4cef2cce87cbe789f5ab5dc
KingNelx <tapiajonel606@gmail.com>
2023-04-28 22:53:41 +0800

    records entity finish

--- src/main/java/com/eHospital/EHospital/Model/Records.java
+++ src/main/java/com/eHospital/EHospital/Model/Records.java
@@ -8,14 +8,26 @@ import jakarta.persistence.Id;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "Records")
-@Entity 
+@Entity
 public class Records {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private @Getter @Setter String id;
+
+    @ElementCollection
+    private @Getter @Setter List <String> findings;
+    @ElementCollection
+    private @Getter @Setter List <String> diagnostics;
+    @ElementCollection
+    private @Getter @Setter List <String> medications;
 }
