3fdc6724d892c19f0847ac82f6dd970ad15cc47c
KingNelx <tapiajonel606@gmail.com>
2023-04-28 10:57:36 +0800

    added attirubutes for records model

--- src/main/java/com/eHospital/EHospital/Model/Records.java
+++ src/main/java/com/eHospital/EHospital/Model/Records.java
@@ -1,5 +1,21 @@
 package com.eHospital.EHospital.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Records")
+@Entity 
 public class Records {
-    
+
 }
