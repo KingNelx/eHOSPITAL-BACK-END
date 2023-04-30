5babf8c4cb712b7b906920042d3abbd04e77b933
KingNelx <tapiajonel606@gmail.com>
2023-04-30 08:21:56 +0800

    records service finish

--- src/main/java/com/eHospital/EHospital/Service/RecordsService.java
+++ src/main/java/com/eHospital/EHospital/Service/RecordsService.java
@@ -1,5 +1,28 @@
 package com.eHospital.EHospital.Service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.eHospital.EHospital.Model.Records;
+
+@Service
 public interface RecordsService {
-    
+
+    // add new records
+    ResponseEntity<Records> addNewRecord(Records newRecords);
+
+    // get all records
+    List<Records> getAllRecords();
+
+    // update records by id
+    ResponseEntity<Records> updateRecords(Records updateRecords, String id);
+
+    // get records by id
+    Optional<Records> getRecordsByID(String id);
+
+    // delete records by id
+    ResponseEntity<String> deleteRecordsById(String id);
 }
