5ecbd417a819c47f362f31a838008e8a64141b41
KingNelx <tapiajonel606@gmail.com>
2023-05-26 01:40:37 +0800

    added more fields in patient entity

--- hospital-management-system/src/main/java/com/hospital/hospitalmanagementsystem/Model/Patient.java
+++ hospital-management-system/src/main/java/com/hospital/hospitalmanagementsystem/Model/Patient.java
@@ -6,11 +6,26 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import org.springframework.data.mongodb.core.mapping.Document;
 import jakarta.persistence.Entity;
-import jakarta.persistence.SequenceGenerator;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 @Document(collection = "Patients")
 public class Patient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private @Getter @Setter String id;
+    private @Getter @Setter String firstName;
+    private @Getter @Setter String lastName;
+    private @Getter @Setter String middleName;
+    private @Getter @Setter String dateOfBirth;
+    private @Getter @Setter String gender;
+    private @Getter @Setter String contactNumber;
+    private @Getter @Setter String email;
+    private @Getter @Setter String address;
 }
