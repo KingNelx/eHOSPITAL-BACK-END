775cd82f49308c62bcd42aae78e842c0683cc738
KingNelx <tapiajonel606@gmail.com>
2023-05-31 01:02:47 +0800

    added records service

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Service/RecordsImpl.java
+++ eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Service/RecordsImpl.java
@@ -2,7 +2,17 @@ package eHOSPITALBACKEND.Service;
 
 import org.springframework.stereotype.Service;
 
+import eHOSPITALBACKEND.Model.Records;
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public interface RecordsImpl {
     
+    // get all records
+    List <Records> getAllRecords();
+
+    // get records by id
+    Optional <Records> getRecordsById(String id);
 }
