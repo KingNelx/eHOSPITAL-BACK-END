30bc6d8430f914a644003b975492eb830140bde0
KingNelx <tapiajonel606@gmail.com>
2023-06-02 09:38:26 +0800

    created an API for creating an admin account

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/AdminRepo.java
+++ eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/AdminRepo.java
@@ -1,11 +1,13 @@
 package eHOSPITALBACKEND.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import java.util.Optional;
 import eHOSPITALBACKEND.Model.Admin;
 
 public interface AdminRepo extends MongoRepository<Admin, String> {
-    Admin findByEmail(String email);
-    Admin findyByUserName(String userName);
-    Admin findyByPassword(String password);
+    Optional<Admin> findByEmail(String email);
+
+    Optional<Admin> findyByUserName(String userName);
+
+    Optional<Admin> findyByPassword(String password);
 }
