f35fcf1bac17314b7d4cd7ce87b8e2fa8ff1b1c0
KingNelx <tapiajonel606@gmail.com>
2023-06-02 09:15:08 +0800

    added validation for admin signup

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/AdminRepo.java
+++ eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/AdminRepo.java
@@ -4,6 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import eHOSPITALBACKEND.Model.Admin;
 
-public interface AdminRepo extends MongoRepository <Admin, String>{
-    
+public interface AdminRepo extends MongoRepository<Admin, String> {
+    Admin findByEmail(String email);
+    Admin findyByUserName(String userName);
+    Admin findyByPassword(String password);
 }
