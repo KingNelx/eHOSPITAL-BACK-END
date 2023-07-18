5e240b7f6dc19e861bb0db1110d2e6eec17effc6
KingNelx <tapiajonel606@gmail.com>
2023-07-18 10:50:06 +0800

    Doctor entity finished

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Entity/Doctor.java
+++ eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Entity/Doctor.java
@@ -18,6 +18,7 @@ public class Doctor {
     private @Getter @Setter String firstName;
     private @Getter @Setter String lastName;
     private @Getter @Setter String contactNumber;
+    private @Getter @Setter String yrsOfExp;
     private @Getter @Setter String email;
     private @Getter @Setter String userName;
     private @Getter @Setter String password;
