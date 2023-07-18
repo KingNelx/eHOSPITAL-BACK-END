ead04424389d1fddfe61d5f7ec68beb855c299cd
KingNelx <tapiajonel606@gmail.com>
2023-07-18 10:44:33 +0800

    patient entity finished

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Entity/Admin.java
+++ eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Entity/Admin.java
@@ -1,4 +1,24 @@
 package eHOSPITALBACKEND.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ElementCollection;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Document(collection = "Admin")
 public class Admin {
+    @Id
+    private @Getter @Setter String id;
+    private @Getter @Setter String firstName;
+    private @Getter @Setter String lastName;
+    private @Getter @Setter String email;
+    private @Getter @Setter String userName;
+    private @Getter @Setter String password;
 }
