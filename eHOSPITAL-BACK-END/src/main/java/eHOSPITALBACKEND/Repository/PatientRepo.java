beba62fda957207ce3c23551c072a7faa5b918e2
KingNelx <tapiajonel606@gmail.com>
2023-07-18 09:36:32 +0800

    reset mvc

--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package eHOSPITALBACKEND.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import eHOSPITALBACKEND.Model.Patient;
-
-
-public interface PatientRepo extends MongoRepository <Patient, String> {
-    
-}
--- eHOSPITAL-BACK-END/src/main/java/eHOSPITALBACKEND/Repository/RecordsRepo.java
+++
@@ -1,9 +0,0 @@
-package eHOSPITALBACKEND.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import eHOSPITALBACKEND.Model.Records;
-
-public interface RecordsRepo extends MongoRepository <Records, String> {
-        
-}
